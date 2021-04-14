f12788e76f3d70bba858a72f1a7767919320a16c
Ater Roma <t.difiori@aterroma.it>
2021-04-14 10:27:52 +0200

    add test service

--- src/app/services/verbali.service.ts
+++ src/app/services/verbali.service.ts
@@ -10,7 +10,8 @@ export class VerbaliService {
 API_URL: string;
 
   constructor(private http_client:HttpClient) {
-    this.API_URL = 'http://interno.aterroma.it/dafne/api';
+    //this.API_URL = 'http://interno.aterroma.it/dafne/api';
+    this.API_URL = 'http://gcdoc.aterroma.it/dafne/api';
   }
 
 
